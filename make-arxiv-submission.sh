180d9da1ee5b3e9983a051f87720e357f15c70d6
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2017-05-18 17:18:38 +0200

    Added arxiv submission bundle script

---
+++ make-arxiv-submission.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create a .zip file containing all files needed for arXiv submission,
+# for easy uploading
+
+master_name=pushout
+
+latexmk -bibtex -pdf ${master_name}
+
+current_date=$(date "+%Y-%m-%d")
+submission_name="${current_date}-arxiv-submission"
+submission_dir_name="${submission_name}-temp"
+
+# create temporary directory of files for arXiv:
+rm -rf $submission_dir_name
+mkdir $submission_dir_name
+cp *.tex ${submission_dir_name}/
+cp ${master_name}.bbl ${submission_dir_name}/
+# remove any files *not* to be included in submission
+cd $submission_dir_name
+rm higher-inductive-semantics.tex
+
+# create zip archive, from inside temp directory so files are at root level of archive
+zip -r ../${submission_name}.zip *
+cd ..
+
+# clean up temp directory
+rm -rf $submission_dir_name
